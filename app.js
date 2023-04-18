475f2e5f09564cf342bb39bae8aae8bb9d029450
Jonathan Herrera <msjona28@outlook.com>
2023-04-18 11:27:12 -0700

    Nueva ruta que ejecuta el metodo getAll y devuelve todos los arrendatarios

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const puerto = 2800;
 
+// Controladores
+const arrendatariosController = require('./controller/arrendatariosController.js');
+
 app.get('/', (req, res) => {
     res.send('Hello world!');
 })
@@ -10,3 +13,5 @@ app.get('/', (req, res) => {
 app.listen(2800, () => {
     console.log(`Escuchando el puerto ${puerto}`);
 })
+
+app.get('/arrendatarios', arrendatariosController.getAll);
