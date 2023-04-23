ccba33651cc88ffef7690bcabdd36ee652b7cf15
Jonathan Herrera <msjona28@outlook.com>
2023-04-23 13:26:02 -0700

    Corrección en nombre de variable que referencia al modelo de las posesiones

--- controller/posesionesController.js
+++ controller/posesionesController.js
@@ -1,26 +1,26 @@
-const propietariosModel = require('../model/posesiones.js');
+const posesionesModel = require('../model/posesiones.js');
 
 const getAll = async function(req, res)
 {
-    let p = propietariosModel.getAll();
+    let p = posesionesModel.getAll();
     await res.json(p);
 }
 
 const getById = async function(req, res)
 {
-    let p = propietariosModel.getById(req.params.id);
+    let p = posesionesModel.getById(req.params.id);
     await res.json(p);
 }
 
 const getPropiedadesByIdPropietario = async function(req, res)
 {
-    let p = propietariosModel.getPropiedadesByIdPropietario(req.params.id);
+    let p = posesionesModel.getPropiedadesByIdPropietario(req.params.id);
     await res.json(p);
 }
 
 const getPropietariosByIdPropiedad = async function(req, res)
 {
-    let p = propietariosModel.getPropietariosByIdPropiedad(req.params.id);
+    let p = posesionesModel.getPropietariosByIdPropiedad(req.params.id);
     await res.json(p);
 }
 
